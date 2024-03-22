5f5322501b77d271700d86a8d40595556530170c
Shehryar Hassan <74443334+SHassan786@users.noreply.github.com>
2024-03-22 14:11:31 +0000

    shehryar finalised schema

--- src/models/question.model.js
+++ src/models/question.model.js
@@ -9,6 +9,9 @@ const questionSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    
+    
+
 });
 
 const Question = mongoose.model("Question", questionSchema, "Questions");
